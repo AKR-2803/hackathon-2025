4d59a6b887c024c135323e76eb2d0abb50755ec0
AKR-2803 <akr28032001@gmail.com>
2025-04-20 10:41:31 -0700

    files uploaded

---
+++ backend/image2base64.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+
+const imageToBase64 = (filePath) => {
+    // read the image file as a buffer
+    const imageBuffer = fs.readFileSync(filePath);
+
+    // xonvert buffer to Base64 string
+    const base64String = imageBuffer.toString('base64');
+
+    // return Base64 string
+    return base64String;
+};
+
+// const base64Url = imageToBase64("./output/page-1.jpeg");
+
+export default imageToBase64;
